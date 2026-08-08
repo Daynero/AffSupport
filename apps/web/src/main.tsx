d427ab4243bae09d2360b38628076678aab26460
Roman <kravchenko.roman.fr@gmail.com>
2026-08-09 01:17:35 +0300

    fix(web): recover from failed application boot

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Root from './Root';
 import './styles.css';
+
+document.documentElement.dataset.appBoot = '2026-08-09.1';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Root />
