4b817ce769cea7121677ad7a1fee64b06945386f
Roman Kravchenko <iamdaynero@gmail.com>
2026-09-21 18:05:53 +0300

    fix: schedule updater once per minute

--- supabase/migrations/20260921180000_workspace_read_relief.sql
+++ supabase/migrations/20260921180000_workspace_read_relief.sql
@@ -29,6 +29,6 @@ where job.jobname = 'wishly-catalog-updater';
 
 select cron.schedule(
   'wishly-catalog-updater',
-  '60 seconds',
+  '* * * * *',
   $cron$select private.invoke_catalog_updater_worker()$cron$
 );
